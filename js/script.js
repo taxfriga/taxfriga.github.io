81df9da149a762eb1320d97ef2f2fd19b25398ff
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-04-04 16:32:01 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,14 +38,14 @@ window.addEventListener('DOMContentLoaded', () => {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
                     // dots: true,
-                    arrows: false,
+                    arrows: true,
                     autoplay: true,
                     autoplaySpeed: 2000
                 }
