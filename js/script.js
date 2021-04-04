8415884ed140630c3cdc49d520cfb2ca5bd69711
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-04-04 15:32:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,18 +15,52 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
     
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1600) {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    });
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 800) {
+    //         $('.pageup').fadeIn();
+    //     } else {
+    //         $('.pageup').fadeOut();
+    //     }
+    // });
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 600) {
+    //         $('.phone').fadeIn();
+    //     } else {
+    //         $('.phone').fadeOut();
+    //     }
+    // });
 
     $("a[href=#up]").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
+    });
+    $(document).ready(function(){
+        $('.carousel__inner').slick({
+            infinite: true,
+            speed: 1200,
+            // adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            fade: true,
+            cssEase: 'linear',
+            prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    infinite: true,
+                    speed: 1200,
+                    // adaptiveHeight: true,
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    fade: true,
+                    cssEase: 'linear',
+                }
+            }
+        ]
+        });
     });
     
 });
