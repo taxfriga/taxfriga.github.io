bc5be9ec1d8fd554720677495c3f68da7c992486
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-05-06 21:37:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-    $('ul.projects__tabs').on('click', 'li:not(.projects__tab_active)', function() {
-        $(this)
-        .addClass('projects__tab_active').siblings().removeClass('projects__tab_active')
-        .closest('div.container').find('div.projects__content').removeClass('projects__content_active').eq($(this).index()).addClass('projects__content_active');
-    });
-
-    $('.questions__item_question').click(function(){
-        // $(this).css('background-color', 'orange')
-        // $('.faq-item-answer').slideToggle(500)
-        $(this)
-        .parents('.questions__item')
-        .find('.questions__item_answer')
-        .slideToggle(500)
-    });
+  $('.carousel__inner').slick({
+    infinite: true,
+    speed: 1200,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-prev-active-2.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-next-active-2.svg"></button>',
+    
+  });
 });
