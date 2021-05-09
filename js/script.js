620b9fd52757f8d13473c28e945637917e2933e7
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-05-09 23:29:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -105,5 +105,22 @@ $(document).ready(function(){
   });
   
   
+  
 
+});
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
 });
