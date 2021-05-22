3c3f67064455ba81dda3e601da3da0f1d3f33f97
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-05-22 11:11:46 +0300

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -9,17 +9,17 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
-
+$mail = new PHPMailer();
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'dncbrand1@gmail.com';                 // Наш логин
+$mail->Password = 'Marianna32';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('dncbrand1@gmail.com', 'Interernye-Kukly');   // От кого письмо 
+$mail->addAddress('interernyekukly.ru@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
