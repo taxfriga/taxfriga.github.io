b1b3c83b8ec0d9754a8dbf24886f1be4f826bfd1
taxfriga <81829151+taxfriga@users.noreply.github.com>
2021-10-26 01:04:08 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
  
-    $('ul.portfolio__tabs').on('click', 'li:not(.portfolio__tab_active)', function() {
-        $(this)
-          .addClass('portfolio__tab_active')
-          .siblings()
-          .removeClass('portfolio__tab_active')
-          .closest('div.container')
-          .find('div.portfolio__content')
-          .removeClass('portfolio__content_active')
-          .eq($(this).index())
-          .addClass('portfolio__content_active');
-    });
-
-    $('.faq__item_guestion').click(function(){
-        $(this).parents('.faq__item').find('.faq__item_answer').slideToggle();
-    });
-
     const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
@@ -28,29 +12,9 @@ $(document).ready(function(){
         menu.classList.remove('active');
     });  
 
-    new WOW().init();
 
 });
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    // dots: true,
-                    arrows: true,
-                    autoplay: true,
-                    autoplaySpeed: 2000
-                }
-            }
-        ]
-    });
-});
-  
+
 
 
