dff55e46cecc2cabb8bbc3a3559697dfb3b828d5
taxfriga <81829151+taxfriga@users.noreply.github.com>
2022-01-23 00:44:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger__active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
 });
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
-
-counters.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
-
-$(document).ready(function(){
-    new WOW().init();
-});
-
