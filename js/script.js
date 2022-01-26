4a378647e0919b0d392325c0532ed79d1a132553
taxfriga <81829151+taxfriga@users.noreply.github.com>
2022-01-26 10:03:18 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger__active');
-        menu.classList.toggle('menu__active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger__active');
-            menu.classList.toggle('menu__active');
-        });
-    });
+$('.carousel__inner').slick({
+    dots: false,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 3.5,
+    slidesToScroll: 1,
+    nextArrow:'<button type="button" class="slick-next"><img src="img/arrow.png"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
 });
